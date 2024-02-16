24cc7c8487e978f03d0365e9538e63362e60bd7c
realrahul01 <dev.ecomed@gmail.com>
2024-02-16 14:59:29 +0530

    commit n

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,35 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
+export class TasksComponent implements OnInit {
+  tasks: { name: string, completed: boolean }[] = [];
+  newTask: string = '';
 
-export class TasksComponent {
-  tasks:string[]=[];
-  newTask:string="";  // two way binding 
+  constructor() { }
 
-
-  addTask(){
-    this.tasks.push(this.newTask)
-    this.newTask=""
+  ngOnInit(): void {
+    // Fetch tasks from storage or initialize empty array
+    const storedTasks = localStorage.getItem('tasks');
+    this.tasks = storedTasks ? JSON.parse(storedTasks) : [];
   }
 
-  deleteTask(task:string){
-    this.tasks=this.tasks.filter(ele=>ele!=task);
-   
-  }
-
-  editTask(task: string): void {
-    const index = this.tasks.indexOf(task); 
-    if (index !== -1) { 
-      const editedTask = prompt('Edit task:', task); 
-      if (editedTask !== null) {
-        this.tasks[index] = editedTask; 
-      }
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push({ name: this.newTask, completed: false });
+      this.newTask = '';
+      this.saveTasks();
     }
   }
 
+  deleteTask(index: number): void {
+    this.tasks.splice(index, 1);
+    this.saveTasks();
+  }
 
+  editTask(index: number): void {
+    const editedTask = prompt('Edit task:', this.tasks[index].name);
+    if (editedTask !== null) {
+      this.tasks[index].name = editedTask;
+      this.saveTasks();
+    }
+  }
+
+  markAsCompleted(index: number): void {
+    this.tasks[index].completed = !this.tasks[index].completed;
+    this.saveTasks();
+  }
+
+  private saveTasks(): void {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 }
