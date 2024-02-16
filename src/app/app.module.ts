5a6f2b65c754fbe1c2de07871ac87e08b945c3e2
realrahul01 <dev.ecomed@gmail.com>
2024-02-16 13:05:43 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+
+export class TasksComponent {
+  tasks:string[]=[];
+  newTask:string="";  // two way binding 
+
+
+  addTask(){
+    this.tasks.push(this.newTask)
+    this.newTask=""
+  }
+
+  deleteTask(task:string){
+    this.tasks=this.tasks.filter(ele=>ele!=task);
+   
+  }
+
+  editTask(task:string){
+    this.newTask=task;
+   
+  }
+
+
+}
